39f34b45d12954b5fc438dd07bebb90e87127e47
Angus Russell <62576601+gussell87@users.noreply.github.com>
2020-06-27 10:56:51 +0930

    Add html skeleton (#8)
    * cleaning up github
    
    * updating github. cleaning

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,40 @@
-// New JS file
-
-// API key for "The dog api" - https://docs.thedogapi.com/
-var apiKey = "804e825d-6365-40ce-99e9-1312b077fc63"
-
-function getDog(dogSearch) {  
-    console.log(dogSearch)
-
+$(".button").on("click", function () {
+    // clearResults();
+    //api key
+    var dogApiKey = "804e825d-6365-40ce-99e9-1312b077fc63";
+    //user input query added to var
+    var breedId = $(".input").val();
+    //forming url depending on input (Andrew to update)
+    var queryURL = ("https://api.thedogapi.com/v1/images/search?apikey=" + dogApiKey + "&q=" + breedId);
+    //check if url working
+    console.log(queryURL);
+    //retreive data
     $.ajax({
-        async: true,
-        crossDomain: true,
-        url: "https://api.thedogapi.com/v1/breeds/search?api_key=804e825d-6365-40ce-99e9-1312b077fc63&q=" + dogSearch,
-        method: "GET",
-    }).done(function (response) {
-        $("#bredFor").text(response[0].bred_for)
-        $("#dogWeight").text(response[0].weight.metric)
-        $("#dogTemp").text(response[0].temperament)
-        console.log(response[0].bred_for)
-        console.log(response);
-    });
-};
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response[0].url);
+        $("img").sr
 
-var srcBtn = $("#srcBtn");
-srcBtn.on("click", function () {
-    event.preventDefault();
-    var dogSearch = $(this).prev().val().trim();
-    console.log(dogSearch);
-    getDog(dogSearch);
+        // var results = response.response.docs;
+        // for (var i = 0; i < breedsReturned; {
+        // var breedDiv = $("<div>");
+
+        //Do info
+        //$("#dogInfo").text("Breed Group: "+ element.breed_group, <br> "Height: "+element.weight.metric, <br> "Life Span: "+ element.life_span, <br> "Weight: " +element.weight.metric)
+
+        // });
+    });
+
+
+    function clearResults() {
+        $(this).closest('form').find("input[type=text], textarea").val("");
+        $("#dogOne").closest('form').find("input[type=text], textarea").val("");
+        $("#dogTwo").closest('form').find("input[type=text], textarea").val("");
+        $("#dogThree").closest('form').find("input[type=text], textarea").val("");
+        $("#dogFour").closest('form').find("input[type=text], textarea").val("");
+        $("#dogTemp").empty();
+
+    };
+});
 });
